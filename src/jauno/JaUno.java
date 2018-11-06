914f19256c9bc245c89d84b7279bc8327396e2b2
delucaa.2022 <delucaa.2022@KHS-C214-24759.kenston.local>
2018-11-06 12:41:18 -0500

    Added sout to Main Class

--- src/jauno/JaUno.java
+++ src/jauno/JaUno.java
@@ -16,7 +16,7 @@ public class JaUno {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        //INITIAL COMMIT f
+        System.out.println("Welcome to Uno! How many players will be playing?");
     }
     
 }
