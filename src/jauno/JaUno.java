51a2d6952d1cf2bf32a6ebada1f9f463006bcb63
shieldsz.2021 <shieldsz.2021@KHS-C214-23834.kenston.local>
2018-11-06 12:31:08 -0500

    changed something

--- src/jauno/JaUno.java
+++ src/jauno/JaUno.java
@@ -16,7 +16,7 @@ public class JaUno {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        //INITIAL COMMIT.fggfdgdsafgds
+        //INITIAL COMMIT f
     }
     
 }
